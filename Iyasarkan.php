0c83024794d480d6850e46545912d9218c8d6be1
IyasarkanProgrammer <muhammadiyasa868@gmail.com>
2026-05-18 18:30:55 +0700

    Update Iyasarkan.php

---
+++ Iyasarkan.php
@@ -0,0 +1,62 @@
+<!-- Keterangan -->
+<div class="card">
+    <h2>Keterangan</h2>
+
+    <h3>Bobot Penilaian</h3>
+
+    <table>
+        <tr>
+            <th>Komponen</th>
+            <th>Bobot</th>
+        </tr>
+
+        <tr>
+            <td>Kehadiran</td>
+            <td>20%</td>
+        </tr>
+
+        <tr>
+            <td>Tugas</td>
+            <td>25%</td>
+        </tr>
+
+        <tr>
+            <td>Project Akhir</td>
+            <td>55%</td>
+        </tr>
+    </table>
+
+    <h3>Range Nilai</h3>
+
+    <table>
+        <tr>
+            <th>Angka</th>
+            <th>Huruf</th>
+        </tr>
+
+        <tr>
+            <td>80 - 100</td>
+            <td>A</td>
+        </tr>
+
+        <tr>
+            <td>70 - 79</td>
+            <td>B</td>
+        </tr>
+
+        <tr>
+            <td>60 - 69</td>
+            <td>C</td>
+        </tr>
+
+        <tr>
+            <td>31 - 59</td>
+            <td>D</td>
+        </tr>
+
+        <tr>
+            <td>0 - 30</td>
+            <td>E</td>
+        </tr>
+    </table>
+</div>
